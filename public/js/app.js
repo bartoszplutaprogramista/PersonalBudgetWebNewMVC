26e6679d79a24762f7c61f29dfc71afff04b0cb8
Bartosz Pluta <bartosz.pluta.programista@gmail.com>
2023-09-05 20:55:34 +0200

    Started adding show hide password in sign up

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@
  * Valid passwords contain at least one letter and one number.
  */
 $.validator.addMethod('validPassword',
-    function(value, element, param) {
+    function (value, element, param) {
 
         if (value != '') {
             if (value.match(/.*[a-z]+.*/i) == null) {
@@ -17,5 +17,5 @@ $.validator.addMethod('validPassword',
 
         return true;
     },
-    'Must contain at least one letter and one number'
+    'Musi zawierać przynajmniej jedną literę i jedną cyfrę'
 );
