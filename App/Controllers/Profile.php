8e22b2da80ab2781111ee96dd472c2bf6c73588c
Bartosz Pluta <bartosz.pluta.programista@gmail.com>
2023-09-29 13:07:31 +0200

    Added settings of the user - edit data

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -60,7 +60,7 @@ class Profile extends Authenticated
     {
         if ($this->user->updateProfile($_POST)) {
 
-            Flash::addMessage('Changes saved');
+            Flash::addMessage('Zmiany zapisane');
 
             $this->redirect('/profile/show');
 
