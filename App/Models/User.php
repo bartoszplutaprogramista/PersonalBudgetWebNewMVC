3f85be76886b63d95d325e54f4650a28bc893714
Bartosz Pluta <bartosz.pluta.programista@gmail.com>
2023-11-05 17:33:29 +0100

    Added Polish subject of an email

--- App/Models/User.php
+++ App/Models/User.php
@@ -231,7 +231,7 @@ class User extends \Core\Model
         $text = View::getTemplate('Password/reset_email.txt', ['url' => $url]);
         $html = View::getTemplate('Password/reset_email.html', ['url' => $url]);
 
-        Mail::send($this->email, 'Password reset', $text, $html);
+        Mail::send($this->email, 'Resetuj hasło', $text, $html);
     }
 
     public static function findByPasswordReset($token)
